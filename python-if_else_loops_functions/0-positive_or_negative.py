b4123483880c2d35a4091bda4dc39f200765b04f
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-29 21:39:04 +0200

    use .format to print message

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-	print(f'{number} is positive\n')
+	print("{:d} is positive".format(number))
 elif number < 0:
-	print(f'{number} is negative\n')
+	print("{:d} is negative".format(number))
 else:
-	print(f'{number} is 0\n')
+	print("0 is zero")
