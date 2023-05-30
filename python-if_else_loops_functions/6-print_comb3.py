d110208eef19275adfa7de10e2ddf94230381159
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-30 11:30:20 +0200

    program that prints all possible different combinations of two digits

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for num in range(90):
+    if num % 10 > num / 10:
+        if num != 89:
+            print("{:02d}, ".format(num), end='')
+        else:
+            print("{:02d}\n".format(num), end='')
