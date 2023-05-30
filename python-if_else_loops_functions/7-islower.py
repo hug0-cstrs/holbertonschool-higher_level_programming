ecc301b8c656c76819eeb589b49057e4cac90518
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-30 12:15:26 +0200

    try to fix last check

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return (True)
-    else:
-        return (False)
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    return False
