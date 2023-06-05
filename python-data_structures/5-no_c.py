016bce80d988709df1f3af5b192fb82edc9e8b64
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-05 18:03:57 +0200

    try to use correctly translate() function

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    translation = str.maketrans('', '', 'cC')
-
-    return my_string.translate(translation)
+    return my_string.translate({ord(c): None for c in "cC"})
