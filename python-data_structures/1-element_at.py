b0a31472cd117f309a5cea9ad45e4c5153774ec3
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-05 16:06:07 +0200

    Create a function that retrieves an element from a list

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+
+    lenght = len(my_list)
+    if idx > lenght:
+        return None
+    return my_list[idx]
