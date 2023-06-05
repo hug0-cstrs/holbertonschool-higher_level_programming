f4f89346cbf6c3709cd0cd338034abf87c583fe6
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-05 20:57:47 +0200

    rename variables

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j != 0:
+    for row in range(len(matrix)):
+        for columns in range(len(matrix[row])):
+            if columns != 0:
                 print(" ", end='')
-            print("{:d}".format(matrix[i][j]), end='')
+            print("{:d}".format(matrix[row][columns]), end='')
         print()
