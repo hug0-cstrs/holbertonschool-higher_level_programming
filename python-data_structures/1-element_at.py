df3378000336c9f51fa0e35d909da09e5c13cf51
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-05 16:07:26 +0200

    modify lenght of my_list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,9 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
 
-    lenght = len(my_list)
+    lenght = len(my_list) - 1
+
     if idx > lenght:
         return None
+
     return my_list[idx]
