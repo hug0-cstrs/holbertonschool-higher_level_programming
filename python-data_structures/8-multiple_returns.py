0fcbc200cdec1d1e5e8f1b971f9ed07e77e461c2
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-06 13:35:43 +0200

    change if condition

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
+    if sentence == 0:
         new_tuple = (len(sentence), None)
         return new_tuple
     new_tuple = (len(sentence), sentence[0])
