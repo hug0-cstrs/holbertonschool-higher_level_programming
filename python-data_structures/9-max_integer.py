234d2392a06c43dac1a3a0d840023d33d562ca70
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-06 14:38:43 +0200

    try to fix last check

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if my_list is None or len(my_list) == 0:
         return None
     max = my_list[0]
-    for i in my_list:
-        if i > max:
-            max = i
+    for i in range(len(my_list)):
+        if max < my_list[i]:
+            max = my_list[i]
     return max
