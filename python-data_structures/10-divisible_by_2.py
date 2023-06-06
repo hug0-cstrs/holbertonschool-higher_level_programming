20035951a24420deda39880d95f1f98e8ecafe25
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-06 15:13:07 +0200

    try to fix last checker

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list is None or len(my_list) == 0:
+    if my_list is None:
         return None
     copy = my_list[:]
-    for i in my_list:
+    for i in range(len(my_list)):
         if copy[i] % 2 == 0:
             copy[i] = True
         else:
