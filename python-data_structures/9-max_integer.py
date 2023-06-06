c7fdc2befe039b63b19185c3a08207ca104bc272
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-06 14:30:55 +0200

    change code without using max()

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,4 +2,8 @@
 def max_integer(my_list=[]):
     if my_list == 0:
         return None
-    return max(my_list)
+    max = my_list[0]
+    for i in my_list:
+        if i > max:
+            max = i
+    return max
