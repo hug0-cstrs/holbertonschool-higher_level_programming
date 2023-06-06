3732131d77f015b18abb1bf13c4aff59785aea00
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-06 13:46:03 +0200

    create a variable to store the length of sentence

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    length = len(sentence)
     if length == 0:
-        tuple_return = (len(sentence), None)
+        tuple_return = (length, None)
         return tuple_return
-    tuple_return = (len(sentence), sentence[0])
+    tuple_return = (length, sentence[0])
     return tuple_return
