bd95abb1ad71594eef6038a53157be16389a1856
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-07 23:13:27 +0200

    create roman_dict

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
-    for i in roman_string:
-        if i is "I":
-            i = 1
-        if i is "V":
-            i = 5
-        if i is "X":
-            i = 10
-        if i is "L":
-            i = 50
-        if i is "C":
-            i = 100
-        if i is "D":
-            i = 500
-        if i is "M":
-            i = 1000
-        add = sum(roman_string[i])
-    return add
+
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
