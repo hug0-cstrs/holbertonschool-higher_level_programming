8c89e2d08ea1e4ea280b867adf3ef59f71bd6217
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-08 13:06:05 +0200

    make file executable

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -12,3 +12,10 @@ def roman_to_int(roman_string):
         "D": 500,
         "M": 1000
     }
+
+    for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i]) == 0:
+            return 0
+    if roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
+        num += roman_dict[roman_string[i]]
+    return num
