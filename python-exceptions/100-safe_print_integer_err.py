fcefdc5a4220d932d51687a2e17910d209a4f8c5
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-11 19:23:15 +0200

    Create a function that prints an integer

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+
+
+value = 89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
