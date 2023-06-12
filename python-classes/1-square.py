86ce11a9519d2a0a2c1ad6bef7cb202f6f98e0bb
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-12 15:39:34 +0200

    try to fix checker

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,12 +2,11 @@
 """
 This is the "Square"  module.
 
-This module provides a simple Square class.
+This module provides a simple Square class with initialize size.
 """
 
 
 class Square:
+    """A class that defines a Square by size"""
     def __init__(self, size):
-        """This is a private instance attribute"""
-
         self.__size = size
