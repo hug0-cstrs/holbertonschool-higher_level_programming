81e76269148d560eeaf6c9fd190ffe551c24b713
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-13 13:35:37 +0200

    move def area over @property

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,12 +3,18 @@
 class Square:
     """A class that defines a Square by size"""
     def __init__(self, size=0):
+        """This is the __init__ method"""
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        """This is a public instance method"""
+        return self.__size * self.__size
 
     @property
     def size(self):
@@ -24,7 +30,3 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
-    def area(self):
-        """This is a public instance method"""
-        return self.__size * self.__size
