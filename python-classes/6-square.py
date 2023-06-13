e7e9ff12eefe948484b7c4b4a00b6ba85740fb4b
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-13 14:58:13 +0200

    reorganize code

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -42,7 +42,7 @@ class Square:
         """This is a public instance method"""
         if \
            type(value) is not tuple or \
-           len(value) is not 2 or type(value[0]) is not int or \
+           len(value) != 2 or type(value[0]) is not int or \
            type(value[1]) is not int or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
@@ -53,10 +53,12 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-        if self.__size == 0:
+        """Print a square method"""
+        if self.__size is 0:
             print("")
         else:
-            for i in range(self.__position):
-                for j in range(self.__position):
-                    print("#", end='')
-                print()
+            if self.__position[1] is not 0:
+                print("\n" * self.__position[1], end='')
+            for i in range(self.__size):
+                print(" " * self.__position[0], end='')
+                print("#" * self.__size)
