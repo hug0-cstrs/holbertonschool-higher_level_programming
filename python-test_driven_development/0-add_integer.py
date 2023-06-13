6b50605e1c5373bb652c06ae3f240ddaa586c192
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-13 22:32:32 +0200

    Create a function that adds 2 integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    '''function that add 2 integers'''
+    if type(a) is int or type(a) is float:
+        if type(b) is int or type(b) is float:
+            return int(a) + int(b)
+        else:
+            raise TypeError("b must be an integer")
+    else:
+        raise TypeError("a must be an integer")
