47c927237b4838ea4f8d6e72624cb13127836aae
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-13 14:35:01 +0200

    change the range of nested loops

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,15 +7,15 @@ This module provides a simple Square class with initialize size.
 
 
 class Square:
-    """A class that defines a Square by size"""
+    """A class that defines a square by size, which defaults 0.
+    Also defines position using a tuple, which defaults (0, 0).
+    Square can also get area, and print square using '#'.
+    When printing, using position, offset on top and left.
+    """
     def __init__(self, size=0, position=(0, 0)):
         """This is the __init__ method"""
         self.position = position
         self.size = size
-
-    def area(self):
-        """This is a public instance method"""
-        return self.__size * self.__size
 
     @property
     def size(self):
@@ -48,11 +48,15 @@ class Square:
         else:
             self.__position = value
 
+    def area(self):
+        """This is a public instance method"""
+        return self.__size * self.__size
+
     def my_print(self):
         if self.__size == 0:
             print("")
         else:
-            for i in range(self.__size):
-                for j in range(self.__size):
+            for i in range(self.__position):
+                for j in range(self.__position):
                     print("#", end='')
                 print()
