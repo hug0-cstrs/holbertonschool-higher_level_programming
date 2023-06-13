b81b40645701c3f06a4894f7d10dcc7fec97d313
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-13 13:33:08 +0200

    add self.value = value

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -16,6 +16,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+        self.value = value
 
         if type(value) is not int:
             raise TypeError("size must be an integer")
