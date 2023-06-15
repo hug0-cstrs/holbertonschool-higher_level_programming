bc6f31c8fbb9a770f99ab2eb0902b92eee16b4c6
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-15 10:34:00 +0200

    reorganize code

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,11 +8,10 @@ It prints "My name is (first) (last)" where (first) and (last) are the args.
 
 
 def say_my_name(first_name, last_name=""):
-    """Print My name is (first) (last) if given, else print error
+    """Print My name is (first) (last) if given, else print error.
     """
-    if type(first_name) is not str or first_name is None:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    elif type(last_name) is not str or last_name is None:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
