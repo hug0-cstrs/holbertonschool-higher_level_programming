349026b2d6fb2028c7568b275898a4de75e27627
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-15 11:05:52 +0200

    try to fix last case

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -15,8 +15,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    elif size < 0 and type(size) is float:
-        raise TypeError("size must be an integer")
+
     for i in range(size):
         for j in range(size):
             print("#", end="")
