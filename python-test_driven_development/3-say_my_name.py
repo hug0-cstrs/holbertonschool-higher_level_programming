b3d066455416371bcccf6fc42e1d808e75be884e
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-15 10:24:42 +0200

    add docstring

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,9 @@ Function that prints My name is <first name> <last name>
 
 
 def say_my_name(first_name, last_name=""):
+    """This function print a string
+    Args: two strings
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
