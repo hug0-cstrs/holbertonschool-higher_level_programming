887db1a05e8dc78a2eefc12b165a52ff4e6f2ce0
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-15 10:31:27 +0200

    try to fix last check

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-'''
-Function that prints My name is <first name> <last name>
-'''
+"""
+This is the "Say My Name" module.
+
+The Say My Name module takes one required parameter and one optional.
+It prints "My name is (first) (last)" where (first) and (last) are the args.
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """This function print a string
-    Args: two strings
+    """Print My name is (first) (last) if given, else print error
     """
-    if type(first_name) is not str:
+    if type(first_name) is not str or first_name is None:
         raise TypeError("first_name must be a string")
-    elif type(last_name) is not str:
+    elif type(last_name) is not str or last_name is None:
         raise TypeError("last_name must be a string")
     else:
-        print("My name is {:s} {:s}".format(first_name, last_name))
+        print("My name is {} {}".format(first_name, last_name))
