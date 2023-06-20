7b1dde4a5aedfd6a9eb1082d8d8c69dad6df8399
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-20 21:25:48 +0200

    delete main test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -12,14 +12,3 @@ class MyList(list):
     def print_sorted(self):
         """ Method that prints the sorted list """
         print(sorted(self))
-
-
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
