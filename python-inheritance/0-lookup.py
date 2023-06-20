254c1cda0e2bb49eb3d0573a48d4b5d1001fbb8e
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-20 21:16:03 +0200

    add description

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""A simple module with a function that returns the list
+of availables attributes and methods of an object
+"""
+
+
 def lookup(obj):
     """ Function that returns the list of available attributes
         and methods of an object
