755998f85a9b2405ce40bfa444aac3a877f44443
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-22 18:41:01 +0200

    Create an empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module with a empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A empty class"""
+    pass
+
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
