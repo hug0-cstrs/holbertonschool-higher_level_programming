a8710181d2ccd44f6d9c87ba7dcc86a99c1ba677
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-22 19:19:28 +0200

    change if condition

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -27,7 +27,7 @@ class BaseGeometry:
             ValueError: if value is less or equal to 0
         """
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
