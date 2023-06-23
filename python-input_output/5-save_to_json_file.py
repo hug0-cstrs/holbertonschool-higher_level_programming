3cde21074e0620cb20bead374bcb5cbb98de9608
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-23 11:59:57 +0200

    add write option

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -13,5 +13,5 @@ def save_to_json_file(my_obj, filename):
 
     Return: nothing
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(json.dumps(my_obj))
