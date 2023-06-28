82445fc2a4fd04a02bb0debe4eeae35a80ec8271
hug0-cstrs <hugo.casteras@outlook.fr>
2023-06-28 09:51:25 +0200

    Update the class Rectangle by adding the public method def to_dictionary(self): that returns the dictionary representation of a Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -149,3 +149,15 @@ class Rectangle(Base):
                     self.x = value
                 elif cle == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Public method that returns the dictionary representation
+        of a Rectangle
+        """
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
